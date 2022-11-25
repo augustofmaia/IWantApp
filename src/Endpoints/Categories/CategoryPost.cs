d3e58a611c4355e7be4b8b528ce3bd450d900baa
Augusto Maia <augustofmaia@hotmail.com>
2022-11-25 10:26:04 -0300

    Concluindo seção 16 - Async vs Sync

--- src/Endpoints/Categories/CategoryPost.cs
+++ src/Endpoints/Categories/CategoryPost.cs
@@ -1,32 +1,29 @@
 ﻿using IWantApp.Domain.Products;
 using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Authorization;
-using static System.Net.WebRequestMethods;
 using System.Security.Claims;
 
 namespace IWantApp.Endpoints.Categories;
 
 public class CategoryPost
-{ 
+{
     public static string Template => "/categories";
-    // O "=>" serve para setar a rota na hora que criar a propriedade 
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handle => Action;
 
     [Authorize(Policy = "EmployeePolicy")]
-    public static IResult Action(CategoryRequest categoryRequest, HttpContext http, ApplicationDbContext context)
+    public static async Task<IResult> Action(CategoryRequest categoryRequest, HttpContext http, ApplicationDbContext context)
     {
         var userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
         var category = new Category(categoryRequest.Name, userId, userId);
 
-        //Tratando o erro 400 Bad Request "Usuario informou dados errados", retornando um padrão "rfc7231"
         if (!category.IsValid)
         {
             return Results.ValidationProblem(category.Notifications.ConvertToProblemDetails());
         }
 
-        context.Categories.Add(category);
-        context.SaveChanges();
+        await context.Categories.AddAsync(category);
+        await context.SaveChangesAsync();
 
         return Results.Created($"/categories/{category.Id}", category.Id);
     }
--- src/Endpoints/Categories/CategoryPut.cs
+++ src/Endpoints/Categories/CategoryPut.cs
@@ -32,4 +32,4 @@ public class CategoryPut
 
         return Results.Ok();
     }
-}}
+}
