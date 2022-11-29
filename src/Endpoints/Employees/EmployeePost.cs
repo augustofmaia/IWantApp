1e1a07857de22392ba28a103bfe04d83365f78fd
Augusto Maia <augustofmaia@hotmail.com>
2022-11-29 09:55:29 -0300

    Concluindo seção 19 - Gerenciamento de cliente

--- src/Endpoints/Employees/EmployeePost.cs
+++ src/Endpoints/Employees/EmployeePost.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IWantApp.Domain.Users;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
 
@@ -11,27 +12,21 @@ public class EmployeePost
     public static Delegate Handle => Action;
 
     [Authorize(Policy = "EmployeePolicy")]
-    public static async Task<IResult> Action(EmployeeRequest employeeRequest, HttpContext http, UserManager<IdentityUser> userManager)
+    public static async Task<IResult> Action(EmployeeRequest employeeRequest, HttpContext http, UserCreator userCreator)
     {
         var userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
-        var newUser = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email };
-        var result = await userManager.CreateAsync(newUser, employeeRequest.Password);
-
-        if (!result.Succeeded)
-            return Results.ValidationProblem(result.Errors.ConvertToProblemDetails());
-
         var userClaims = new List<Claim>
         {
             new Claim("EmployeeCode", employeeRequest.EmployeeCode),
             new Claim("Name", employeeRequest.Name),
             new Claim("CreatedBy", userId),
         };
+        (IdentityResult identity, string userId) result =
+            await userCreator.Create(employeeRequest.Email, employeeRequest.Password, userClaims);
 
-        var claimResult = await userManager.AddClaimsAsync(newUser, userClaims);
+        if (!result.identity.Succeeded)
+            return Results.ValidationProblem(result.identity.Errors.ConvertToProblemDetails());
 
-        if (!claimResult.Succeeded)
-            return Results.BadRequest(claimResult.Errors.First());
-
-        return Results.Created($"/employees/{newUser.Id}", newUser.Id);
+        return Results.Created($"/employees/{result.userId}", result.userId);
     }
 }
